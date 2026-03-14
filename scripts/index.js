35ff76102c3c751da211c79b9867e5a887c3b8ca
Rochiz <ruzarate31@gmail.com>
2026-03-13 20:31:32 -0500

    Implement edit profile modal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,41 @@ const initialCards = [
 initialCards.forEach(function (card) {
   console.log(card.name);
 });
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-popup");
+const closeButton = editProfileModal.querySelector(".popup__close");
+const editProfileForm = document.querySelector("#edit-profile-form");
+const nameInput = document.querySelector(".popup__input_type_name");
+const descriptionInput = document.querySelector(
+  ".popup__input_type_description",
+);
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+function fillProfileForm() {
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editProfileModal);
+}
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+
+  closeModal(editProfileModal);
+}
+editProfileButton.addEventListener("click", handleOpenEditModal);
+
+closeButton.addEventListener("click", function () {
+  closeModal(editProfileModal);
+});
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
